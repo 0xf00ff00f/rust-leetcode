dc1956bc99e56db372c18d604800e9dcb3ae7f0a
0xf00ff00f <fzorto@gmail.com>
2026-05-01 10:16:29 -0300

    sum_of_square_numbers: try Fermat

--- src/sum_of_square_numbers.rs
+++ src/sum_of_square_numbers.rs
@@ -15,7 +15,7 @@ const SQUARES: [i32; MAX_N + 1] = generate_squares();
 pub struct Solution {}
 
 impl Solution {
-    pub fn judge_square_sum(num: i32) -> bool {
+    pub fn judge_square_sum_brute(num: i32) -> bool {
         fn is_perfect_square(num: i32) -> bool {
             SQUARES.binary_search(&num).is_ok()
         }
@@ -28,8 +28,34 @@ impl Solution {
             }
         }
         false
-        // TODO: should be using this instead:
-        // https://en.wikipedia.org/wiki/Fermat%27s_theorem_on_sums_of_two_squares
+    }
+
+    pub fn judge_square_sum(num: i32) -> bool {
+        // https://en.wikipedia.org/wiki/Sum_of_two_squares_theorem
+        let mut n = num;
+        if n == 0 {
+            return true;
+        }
+        // get rid of powers of 2
+        while (n % 2) == 0 {
+            n /= 2
+        }
+        // TODO: need a table of pythagorean primes
+        let mut p = 3;
+        while n > 1 {
+            let mut e = 0;
+            while n % p == 0 {
+                n /= p;
+                e += 1;
+            }
+            if e > 0 && (p % 4) == 3 {
+                if e % 2 != 0 {
+                    return false;
+                }
+            }
+            p += 2;
+        }
+        true
     }
 }
 
@@ -41,6 +67,13 @@ mod tests {
     fn test_judge_square_sum() {
         assert!(Solution::judge_square_sum(0));
         assert!(Solution::judge_square_sum(5));
+        assert!(Solution::judge_square_sum(2));
         assert!(!Solution::judge_square_sum(3));
+        assert!(!Solution::judge_square_sum(21));
+        assert!(Solution::judge_square_sum(45));
+        assert!(Solution::judge_square_sum(2450));
+        for i in 2..1000 {
+            assert!(Solution::judge_square_sum_brute(i) == Solution::judge_square_sum(i));
+        }
     }
 }
